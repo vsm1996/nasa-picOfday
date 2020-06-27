9e9d2ddcb868fe52333c2611572595a36b870126
Vanessa Martin <vanessa.s.martin96@gmail.com>
2020-06-27 12:31:29 -0700

    adjusted picture styling

--- src/components/picOfDay/picOfDay.jsx
+++ src/components/picOfDay/picOfDay.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import './picOfDay.css'
 
 const PicOfDay = (props) => {
     const { copyright, title, explanation, url, date } = props.pictureData
     return (
         <div>
-            <h1>{title}</h1>
-            <h2> - {copyright}</h2>
-            <img src={url} alt="" srcSet="" />
-            <h6>{date}</h6>
-            <p> {explanation} </p>
+            <h1 className="title">{title}</h1>
+            <h2 className="copyright"> - {copyright}</h2>
+            <img className="image" src={url} alt="" srcSet="" />
+            <h6 className="date">{date}</h6>
+            <p className="explanation"> {explanation} </p>
         </div>
     )
 }
